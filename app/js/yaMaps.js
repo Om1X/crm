9830acc458e2af94224625ce0242b2c3d18fdcc7
Om1X <glomix@inbox.ru>
2017-05-19 09:32:28 +0300

    - добавлен выбор/удаление категорий - добавлено скрытие акции-заглушки - пофикшен конфиг галп

--- app/js/yaMaps.js
+++ app/js/yaMaps.js
@@ -12,79 +12,152 @@ ymaps.ready(function () {
 
     }
 
-    function getInfo() {
-        var addrInputVal = addressInput.value,
-            metroList = '',
-            metroCounter = 3,
-            metroInput = document.getElementById('metro'),
-            coorInput = document.getElementById('coordinates');
-        ymaps.geocode(addrInputVal).then(function (res) { // Ковертация адреса в координаты
 
-            coordinates = res.geoObjects.get(0).geometry.getCoordinates();
-            // поиск ближайших станций и вывод в поле "Метро"
-            ymaps.geocode(coordinates, {
-                kind: 'metro',
-                results: metroCounter
-            }).then(function (met) {
-                met.geoObjects.options.set('preset', 'islands#redCircleIcon');
-                met.geoObjects.each(function (obj) {
-                    if (obj.properties.get('name').split('метро ')[1]) {
-                        metroList += obj.properties.get('name').split('метро ')[1] + '|';
+    // сделать общий блок для прослушки и навешать обработчики для перехвата событий
+
+
+    function addressSuggest(id) {
+        var sInput = document.getElementById(id);
+        if (!sInput) return;
+        var parentBlock = sInput.parentNode,
+            ul = document.createElement('ul');
+        parentBlock.appendChild(ul);
+        ul.classList.add('suggest');
+
+        function getInfo() {
+            if (addressInput.value.length < 5) return;
+            ul.style.display = 'none';
+            console.log(event.target);
+            var addrInputVal = addressInput.value,
+                metroList = '',
+                metroCounter = 3,
+                metroInput = document.getElementById('metro'),
+                coorInput = document.getElementById('coordinates');
+            ymaps.geocode(addrInputVal).then(function (res) { // Ковертация адреса в координаты
+
+                coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+                // поиск ближайших станций и вывод в поле "Метро"
+                ymaps.geocode(coordinates, {
+                    kind: 'metro',
+                    results: metroCounter
+                }).then(function (met) {
+                    met.geoObjects.options.set('preset', 'islands#redCircleIcon');
+                    met.geoObjects.each(function (obj) {
+                        if (obj.properties.get('name').split('метро ')[1]) {
+                            metroList += obj.properties.get('name').split('метро ')[1] + '|';
+                        } else {
+                            metroList += obj.properties.get('name').split('станция ')[1] + '|';
+                        }
+                    });
+
+                    metroInput.value = metroList.split('|').splice(0, 3).join(', ');
+                    coorInput.value = coordinates;
+                });
+
+                // Преобразуем координаты в адрес
+                ymaps.geocode(coordinates).then(function (adr) {
+                    var address;
+                    address = adr.geoObjects.get(0).properties.get('description') + ", ";
+                    address += adr.geoObjects.get(0).properties.get('name');
+                    document.getElementById('address').value = address;
+                    // ставим балун который обозначает точку нашего адреса на карте
+                    myGeoObject = new ymaps.GeoObject({
+                        // Описание геометрии.
+                        geometry: {
+                            type: "Point",
+                            coordinates: coordinates
+                        },
+                        // Свойства.
+                        properties: {
+                            // Контент метки.
+                            // iconContent: adr.geoObjects.get(0).properties.get('name')
+                            // hintContent: 'Ну давай уже тащи'
+                        }
+                    }, {
+                        // Опции.
+                        // Иконка метки будет растягиваться под размер ее содержимого.
+                        preset: 'islands#blueStretchyIcon',
+                        // Метку можно перемещать.
+                        draggable: true
+                    });
+
+                    myMap.geoObjects.remove(myMap.geoObjects.get(0));
+                    myMap.geoObjects.add(myGeoObject);
+                    myMap.setCenter(coordinates);
+                    myGeoObject.events.add('dragend', function () {
+                        coorInput.value = myGeoObject.geometry.getCoordinates();
+                    })
+
+                });
+
+            }, function (err) {
+                console.log(err);
+            });
+        }
+
+
+        function searchAddress() {
+            if (addressInput.value.length < 5) {
+                ul.innerHTML = '';
+                ul.style.display = 'none';
+                return;
+            }
+
+            var xhr = new XMLHttpRequest(),
+                search = addressInput.value,
+                url = 'https://geocode-maps.yandex.ru/1.x/?format=json&results=5&geocode=' + search,
+                res;
+
+            console.log(url);
+            xhr.open('GET', url, true);
+
+
+            xhr.send();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState !== 4) return;
+
+                if (xhr.status !== 200) {
+                    console.log(xhr.status + ': ' + xhr.statusText);
+                } else {
+                    res = JSON.parse(xhr.responseText).response.GeoObjectCollection.featureMember;
+                    if (res.length === 0 || !sInput) {
+                        ul.innerHTML = '';
+                        ul.style.display = 'none';
                     } else {
-                        metroList += obj.properties.get('name').split('станция ')[1] + '|';
+                        ul.style.display = 'block';
+                        ul.innerHTML = '';
+                        res.forEach(function (item, i, res) {
+                            var li = document.createElement('li');
+                            ul.appendChild(li);
+                            li.innerText = res[i].GeoObject.description + ', ' + res[i].GeoObject.name;
+                        });
                     }
-                });
+                }
+            };
+        }
 
-                metroInput.value = metroList.split('|').splice(0, 3).join(', ');
-                coorInput.value = coordinates;
-            });
+        function hide() {
+            setTimeout(function () {
+                ul.innerHTML = '';
+                ul.style.display = 'none';
+            }, 150);
+        }
 
-            // Преобразуем координаты в адрес
-            ymaps.geocode(coordinates).then(function (adr) {
-                var address;
-                address = adr.geoObjects.get(0).properties.get('description') + ", ";
-                address += adr.geoObjects.get(0).properties.get('name');
-                document.getElementById('address').value = address;
-                // ставим балун который обозначает точку нашего адреса на карте
-                myGeoObject = new ymaps.GeoObject({
-                    // Описание геометрии.
-                    geometry: {
-                        type: "Point",
-                        coordinates: coordinates
-                    },
-                    // Свойства.
-                    properties: {
-                        // Контент метки.
-                        // iconContent: adr.geoObjects.get(0).properties.get('name')
-                        // hintContent: 'Ну давай уже тащи'
-                    }
-                }, {
-                    // Опции.
-                    // Иконка метки будет растягиваться под размер ее содержимого.
-                    preset: 'islands#blueStretchyIcon',
-                    // Метку можно перемещать.
-                    draggable: true
-                });
+        function suggestClick() {
+            ul.style.display = 'none';
+            console.log(event.target.innerText);
+            addressInput.value = event.target.innerText;
+            getInfo();
+        }
 
-                myMap.geoObjects.remove(myMap.geoObjects.get(0));
-                myMap.geoObjects.add(myGeoObject);
-                // coorInput.value = myMap.geoObjects.get(0).geometry._coordinates;
-                // myPlacemark = new ymaps.Placemark(coordinates);
-                // console.log(myPlacemark);
-                // myMap.geoObjects.add(myPlacemark);
-                myMap.setCenter(coordinates);
 
-                myGeoObject.events.add('dragend', function () {
-                    coorInput.value = myGeoObject.geometry._coordinates;
-                })
+        ul.addEventListener('click', suggestClick);
+        addressInput.addEventListener('blur', hide);
+        addressInput.addEventListener('keyup', searchAddress);
 
-            });
 
-        }, function (err) {
-            console.log(err);
-        });
     }
 
     crMap();
-    addressInput.addEventListener('change', getInfo);
+    addressSuggest('address');
 });
